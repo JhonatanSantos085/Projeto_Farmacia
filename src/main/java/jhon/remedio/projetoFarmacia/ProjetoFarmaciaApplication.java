6bda5c70ccebd74c6158a6a017d85f78bd3df419
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-08-19 20:22:50 -0300

    .

---
+++ src/main/java/jhon/remedio/projetoFarmacia/ProjetoFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package jhon.remedio.projetoFarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jhon/remedio/projetoFarmacia/ProjetoFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package jhon.remedio.projetoFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
