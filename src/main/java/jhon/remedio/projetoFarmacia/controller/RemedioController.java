611a8900597170fca38415173a70d2f06db03400
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-08-21 19:47:38 -0300

    Atualizei a classe remedio

--- src/main/java/jhon/remedio/projetoFarmacia/controller/RemedioController.java
+++ src/main/java/jhon/remedio/projetoFarmacia/controller/RemedioController.java
@@ -1,12 +1,13 @@
 package jhon.remedio.projetoFarmacia.controller;
 
 
+import jhon.remedio.projetoFarmacia.service.RemedioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/remedio")
+@RequestMapping("/remedios_api")
 public class RemedioController {
 
     @Autowired
--- src/main/java/jhon/remedio/projetoFarmacia/model/Remedio.java
+++ src/main/java/jhon/remedio/projetoFarmacia/model/Remedio.java
@@ -1,23 +1,36 @@
 package jhon.remedio.projetoFarmacia.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
+@EqualsAndHashCode(of = "id")
+@Entity(name = "remedio")
 public class Remedio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    private Via via;
+
+    private String lote;
+    private String quantidade;
+    private String validade;
+
+    @Enumerated(EnumType.STRING)
+    private Laboratorio laboratorio;
+
+
 
 }
